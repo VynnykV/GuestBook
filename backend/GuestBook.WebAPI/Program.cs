1022098a3e4ca8892274bfa4946de290b626cb6f
Vitaliy <vitalik.vinnik.05@gmail.com>
2023-07-11 01:17:04 +0300

    add generic exception handler

--- backend/GuestBook.WebAPI/Program.cs
+++ backend/GuestBook.WebAPI/Program.cs
@@ -1,5 +1,6 @@
 using GuestBook.DAL;
 using GuestBook.WebAPI.Extensions;
+using GuestBook.WebAPI.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +23,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<GenericExceptionHandlerMiddleware>();
 
 app.MigrateDB();
 
